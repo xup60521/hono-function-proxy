dc79394196d6f2403dbf5d01bb1f59f45e5660fa
xup60521 <84887662+xup60521@users.noreply.github.com>
2026-01-17 15:59:53 +0800

    update: return text is always the case

--- index.ts
+++ index.ts
@@ -5,13 +5,7 @@ const app = new Hono();
 app.post("/", async (c) => {
     const { url, fetchOptions } = await c.req.json() as { url: string; fetchOptions: {} };
     const response = await fetch(url, fetchOptions);
-    const contentType = response.headers.get("content-type");
-    
-    if (contentType?.includes("application/json")) {
-        return c.json(await response.json());
-    }
-    
-    return c.body(await response.text());
+    return c.body(await response.text())
 });
 
 export default app;
